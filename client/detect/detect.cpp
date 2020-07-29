3c96b182585b13f31e4ae7587915b9166cb24367
han <13378766179@163.com>
2020-07-29 04:38:13 -0700

    finish the class of detect

---
+++ client/detect/detect.cpp
@@ -0,0 +1,103 @@
+#include"detect.h"
+
+detect::detect()
+{
+    net = detectNet::Create(detectNet::SSD_MOBILENET_V2);
+    if (!net)
+    {
+        printf("failed to load image recognition network\n");
+        return ;
+    }
+    camera_left = gstCamera::Create("0");
+    camera_right = gstCamera::Create("1");
+    imgRGBA_left = NULL;
+    imgRGBA_right = NULL;
+}
+
+detect::~detect()
+{
+    SAFE_DELETE(camera_left);
+    SAFE_DELETE(camera_right);
+}
+
+void detect::CaptureRGBA()
+{
+    if (!camera_left->CaptureRGBA(&imgRGBA_left, 1000, 1))
+        printf("detectnet-camera:  failed to capture RGBA image from camera\n");
+
+    if (!camera_right->CaptureRGBA(&imgRGBA_right, 1000, 1))
+        printf("detectnet-camera:  failed to capture RGBA image from camera\n");
+}
+
+void detect::detect_left()
+{
+    numDetections = net->Detect(imgRGBA_left, camera_left->GetWidth(), camera_left->GetHeight(), &detections, detectNet::OVERLAY_NONE);
+}
+
+const char *detect::Get_ID(int index)
+{
+    if(index>=numDetections)
+    {
+        return "none";
+    }
+    return net->GetClassDesc(detections[index].ClassID);
+}
+
+void detect::Get_Pos(int index, float &top, float &bottom, float &left, float &right)
+{
+    if(index>=numDetections)
+    {
+        return ;
+    }
+    top = detections[index].Top;
+    bottom = detections[index].Bottom;
+    left = detections[index].Left;
+    right = detections[index].Right;
+}
+
+void detect::OverLay_Left(int index)
+{
+    if(index>=numDetections)
+    {
+        return ;
+    }
+    const uint32_t overlayFlags = detectNet::OverlayFlagsFromStr("box");/*label,conf*/
+    detectNet::Detection detection = detections[index];
+    net->Overlay(imgRGBA_left, imgRGBA_left, camera_left->GetWidth(), camera_left->GetHeight(), &detection, 1, overlayFlags);
+}
+
+void detect::Save_RGBA(string left_str,string right_str)
+{
+    char *left_addr=(char*)left_str.data();
+    char *right_addr=(char*)right_str.data();
+    if( !saveImageRGBA(left_addr, (float4*)imgRGBA_left, camera_left->GetWidth(), camera_left->GetHeight(), 255.0f) )
+		printf("detectnet-console:  failed saving %ix%i image to '%s'\n", camera_left->GetWidth(), camera_left->GetHeight(), "outL.jpg");
+
+	//save the right RGBA
+	if( !saveImageRGBA(right_addr, (float4*)imgRGBA_right, camera_right->GetWidth(), camera_right->GetHeight(), 255.0f) )
+		printf("detectnet-console:  failed saving %ix%i image to '%s'\n", camera_left->GetWidth(), camera_left->GetHeight(), "outR.jpg");
+}
+
+int main()
+{
+    detect my_detect=detect();
+    while(1)
+    {
+    my_detect.CaptureRGBA();
+    my_detect.detect_left();
+    for(int i=0;i<my_detect.numDetections;i++)//my_detect.numDetections means the nums of detection
+    {
+        cout<<my_detect.Get_ID(i);
+        
+        float top,bottom,left,right;
+        my_detect.Get_Pos(i,top,bottom,left,bottom);
+        
+        cout<<"top:"<<top<<"bottom:"<<bottom<<"left:"<<left<<"right:"<<right;
+    }
+    //TODO:stereo to know the distance
+    //TODO:we have to know which detection needs to overlay
+    my_detect.OverLay_Left(0);
+    my_detect.Save_RGBA("out1.jpg","out2.jpg");
+    //TODO:we need to trans the picture from jetbot to server
+    }
+}
---
+++ client/detect/detect.h
@@ -0,0 +1,33 @@
+#include <jetson-inference/detectNet.h>
+#include <jetson-utils/loadImage.h>
+#include <jetson-utils/imageIO.h>
+#include <jetson-utils/gstCamera.h>
+#include <opencv2/opencv.hpp>
+#include <string>
+
+using namespace std;
+class detect
+{
+private:
+    detectNet *net;
+
+    gstCamera *camera_left;
+    gstCamera *camera_right;
+
+    float *imgRGBA_left;
+    float *imgRGBA_right;
+
+    detectNet::Detection *detections;
+
+public:
+    int numDetections;
+    detect();
+    ~detect();
+    void CaptureRGBA();
+    void detect_left();
+    const char *Get_ID(int index);
+    void Get_Pos(int index, float &top, float &down, float &left, float &right);
+    void OverLay_Left(int index);
+    void Save_RGBA(string left_str,string right_str);
+};
+
