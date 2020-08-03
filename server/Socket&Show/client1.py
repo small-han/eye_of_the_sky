bd4c99356fcde9540219c5847a20ccfe8001a1e5
Herschel-Yu <52918480+Herschel-Yu@users.noreply.github.com>
2020-08-03 18:59:05 +0800

    Update client1.py

--- server/Socket&Show/client1.py
+++ server/Socket&Show/client1.py
@@ -20,41 +20,41 @@ class Receive:
     def receive_data(self):
         # client = socket.socket()  # 声明socket类型，同时生成socket连接对象
         # client.connect((self.IP,self.port))  # 链接服务器的ip + 端口
-        
-        client = socket.socket()  # 1.声明协议类型，同时生成socket链接对象
-        client.bind((self.IP, self.port))  # 绑定要监听端口=(服务器的ip地址+任意一个端口)
-        client.listen(5)  # 监听
-        print("waiting for connection...")
-        conn, addr = client.accept()  # 等电话打进来
-            # conn就是客户端连过来而在服务器端为其生成的一个连接实例
-        print("收到来自{}请求".format(addr))
-        
-        BASE_DIR = self.DIR  # 获得当前目录
-        
-        command=['pic.png','info.csv','break']
-        for i in range(3):
-            msg = command[i]  # 获得要向服务端发送的信息，字符串格式
-            if len(msg) == 0:
-                continue
-        
-            conn.sendall(msg.encode('utf-8'))
-            # client.send(msg.encode("utf-8"))      # 将字符串格式编码成bytes，发送
-            if msg == 'break':
-                break
-            data = conn.recv(1024)  # 接收服务端返回的内容
-            if len(str(data, 'utf-8').split('|')) == 2:  # 如果返回的字符串长度为2，说明针对的任务2，从服务端传回一张图片
-                filename, filesize = str(data, 'utf8').split('|')  # 获得指定图像的名称，图像大小
-                path = os.path.join(BASE_DIR, filename)  # 指定图像的保存路径
-                filesize = int(filesize)  # 图像大小转换成整形
-        
-                f = open(path, 'ab')  # 以二进制格式打开一个文件用于追加。如果该文件不存在，创建新文件进行写入。
-                has_receive = 0  # 统计接收到的字节数
-                while has_receive != filesize:
-                    data1 = conn.recv(1024)  # 一次从服务端接收1024字节的数据
-                    f.write(data1)  # 写入
-                    has_receive += len(data1)  # 更新接收到的字节数
-                f.close()  # 关闭文件
-            print("recv:", data.decode())
+        while(1):
+            client = socket.socket()  # 1.声明协议类型，同时生成socket链接对象
+            client.bind((self.IP, self.port))  # 绑定要监听端口=(服务器的ip地址+任意一个端口)
+            client.listen(5)  # 监听
+            print("waiting for connection...")
+            conn, addr = client.accept()  # 等电话打进来
+                # conn就是客户端连过来而在服务器端为其生成的一个连接实例
+            print("收到来自{}请求".format(addr))
+
+            BASE_DIR = self.DIR  # 获得当前目录
+
+            command=['pic.png','info.csv','break']
+            for i in range(3):
+                msg = command[i]  # 获得要向服务端发送的信息，字符串格式
+                if len(msg) == 0:
+                    continue
+
+                conn.sendall(msg.encode('utf-8'))
+                # client.send(msg.encode("utf-8"))      # 将字符串格式编码成bytes，发送
+                if msg == 'break':
+                    break
+                data = conn.recv(1024)  # 接收服务端返回的内容
+                if len(str(data, 'utf-8').split('|')) == 2:  # 如果返回的字符串长度为2，说明针对的任务2，从服务端传回一张图片
+                    filename, filesize = str(data, 'utf8').split('|')  # 获得指定图像的名称，图像大小
+                    path = os.path.join(BASE_DIR, filename)  # 指定图像的保存路径
+                    filesize = int(filesize)  # 图像大小转换成整形
+
+                    f = open(path, 'ab')  # 以二进制格式打开一个文件用于追加。如果该文件不存在，创建新文件进行写入。
+                    has_receive = 0  # 统计接收到的字节数
+                    while has_receive != filesize:
+                        data1 = conn.recv(1024)  # 一次从服务端接收1024字节的数据
+                        f.write(data1)  # 写入
+                        has_receive += len(data1)  # 更新接收到的字节数
+                    f.close()  # 关闭文件
+                print("recv:", data.decode())
         
         client.close()
 
