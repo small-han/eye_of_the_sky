2e206697a6784ad7c82e3e2c8e6c040c67bf5a7b
Herschel-Yu <52918480+Herschel-Yu@users.noreply.github.com>
2020-08-08 09:14:48 +0800

    Add files via upload

--- server/Socket&Show/web_server.py
+++ server/Socket&Show/web_server.py
@@ -7,33 +7,38 @@ Created on Thu Aug  6 23:32:48 2020
 
 import asyncio
 import websockets
-import numpy as np
+#Imageprocessing library
 import os
 import nest_asyncio
 nest_asyncio.apply()
 
 async def Operate_soc():
-    uri = "ws://121.40.165.18:8800"
+    # uri = "ws://121.40.165.18:8800"
+    uri = "ws://localhost:6668"
     async with websockets.connect(uri) as websocket:
         while True:
             greet = await websocket.recv()
+            greet = greet.decode('utf-8')
             print(greet)
             filename = await websocket.recv()
+            filename = filename.decode('utf-8')
             print(filename)
             filesize = await websocket.recv()
+            filesize = int(filesize.decode('utf-8'))
             print(filesize)
-            BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pics')
+            
+            # BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pics')
+            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
             path = os.path.join(BASE_DIR, filename)
+            f = open(path, 'ab')  # 以二进制格式打开一个文件用于追加。如果该文件不存在，创建新文件进行写入。
+            data =await websocket.recv()  # 一次从服务端接收1024字节的数据
+            f.write(data)#.decode())  # 写入
+            f.close()  # 关闭文件
+            print("receive successfully!")
 
-            f = open(path, 'ab') 
-            data =await websocket.recv() 
-            f.write(data)
-            f.close() 
 
-
-#run the client untill the function complete
 asyncio.get_event_loop().run_until_complete(Operate_soc())
-
+asyncio.get_event_loop().run_forever()
 
 
 
@@ -65,13 +70,33 @@ asyncio.get_event_loop().run_until_complete(Operate_soc())
 # async def hello():
 #     uri = "ws://121.40.165.18:8800"
 #     async with websockets.connect(uri) as websocket:
-#         # name = "yu"
+#         name = "yu"
 
-#         # await websocket.send(name)
-#         # print(f"> {name}")
+#         await websocket.send(name)
+#         print(f"> {name}")
 
 #         greeting = await websocket.recv()
 #         print(f"< {greeting}")
 
 # asyncio.get_event_loop().run_until_complete(hello())
 # asyncio.get_event_loop().run_forever()
+
+
+
+# import asyncio
+# import websockets
+# import nest_asyncio
+# nest_asyncio.apply()
+
+# async def hello():
+#     uri = "ws://localhost:8765"
+#     async with websockets.connect(uri) as websocket:
+#         name = input("What's your name? ")
+
+#         await websocket.send(name)
+#         print(f"> {name}")
+
+#         greeting = await websocket.recv()
+#         print(f"< {greeting}")
+
+# asyncio.get_event_loop().run_until_complete(hello())
